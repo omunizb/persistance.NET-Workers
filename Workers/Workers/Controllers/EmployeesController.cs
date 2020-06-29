7a2729ad818068486db8f663e4163dd56f32e320
omunizb <oriolmuniz@gmail.com>
2020-06-29 11:47:36 +0200

    mySQL persistance ready

---
+++ Workers/Workers/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Workers.Models;
+
+namespace Workers.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            EmployeeContext context = HttpContext.RequestServices.GetService(typeof(Workers.Models.EmployeeContext)) as EmployeeContext;
+
+            return View(context.GetAllEmployees());
+        }
+    }
+}
---
+++ Workers/Workers/Models/EmployeeContext.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Workers.Models
+{
+    public class EmployeeContext
+    {
+        public string ConnectionString { get; set; }
+
+        public EmployeeContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> list = new List<Employee>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from worker where id < 10", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new Employee()
+                        {
+                            Id = Convert.ToInt64(reader["id"]),
+                            Name = reader["name"].ToString(),
+                            Surname = reader["surname"].ToString(),
+                            Job = reader["job"].ToString(),
+                            Salary = float.Parse(reader["salary"].ToString(), CultureInfo.InvariantCulture.NumberFormat),
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
